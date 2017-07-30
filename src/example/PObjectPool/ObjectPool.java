f775b329760dc20cae0e3e4a4186d1ee40331ecc
fetissovN <fetissov.n@gmail.com>
2017-07-30 14:58:12 +0300

    ObjectPool

--- src/example/PObjectPool/ObjectPool.java
+++ src/example/PObjectPool/ObjectPool.java
@@ -1,4 +1,41 @@
 package example.PObjectPool;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class ObjectPool {
+    public static void main(String[] args) {
+        ObjectPoolClass objectPool = new ObjectPoolClass();
+        PooledObject pooledObject = objectPool.getPooledObject();
+        objectPool.releasePooledObject(pooledObject);
+    }
+}
+
+class PooledObject{
+
+}
+
+class ObjectPoolClass{
+
+    List<PooledObject> free = new LinkedList<>();
+    List<PooledObject> used = new LinkedList<>();
+
+    public PooledObject getPooledObject(){
+        if (free.isEmpty()){
+            PooledObject pooledObject = new PooledObject();
+            free.add(pooledObject);
+            return pooledObject;
+        }else {
+            PooledObject pooledObject = free.get(0);
+            used.add(pooledObject);
+            free.remove(pooledObject);
+            return pooledObject;
+        }
+    }
+
+    public void releasePooledObject(PooledObject pooledObject){
+        used.remove(pooledObject);
+        free.add(pooledObject);
+    }
+
 }
