52d6cfef0085eb2b89fd1cbf4580155d74126433
fetissovN <fetissov.n@gmail.com>
2018-02-12 16:24:13 +0300

    test project 1

---
+++ src/example/TestProjectITRex/test1/Logic.java
@@ -0,0 +1,135 @@
+package example.TestProjectITRex.test1;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Logic {
+
+    private String text;
+    private String C = "c";
+
+    private String A = "a";
+    private String AN = "an";
+    private String THE = "the";
+
+    public Logic(String input) {
+        this.text = input;
+    }
+
+    public String doJob() {
+        String[] splitedText = text.split(" ");
+        StringBuilder withoutC= checkC(splitedText);
+
+        String[] withoutCArray  = withoutC.toString().split(" ");
+        StringBuilder doubleRemovedText = checkDouble(withoutCArray);
+
+        String[] doubleRemovedTextArray = doubleRemovedText.toString().split(" ");
+        StringBuilder textWithNoE = removerLastLetter(doubleRemovedTextArray);
+
+        StringBuilder finalText = removeArticles(text, textWithNoE);
+        return finalText.toString();
+    }
+
+    private StringBuilder checkC(String[] splitedText){
+
+        StringBuilder totalString = new StringBuilder();
+        for (int i = 0; i < splitedText.length; i++) {
+            StringBuilder sBuilder = new StringBuilder(splitedText[i]);
+
+            while (true) {
+                int c = sBuilder.indexOf(C);
+                if (c != -1) {
+                    if (c == sBuilder.length()-1){
+                        sBuilder.replace(c, c + 1, "k");
+                        totalString.append(sBuilder + " ");
+                        break;
+                    }
+                    if (!String.valueOf(sBuilder.charAt(c + 1)).equals("i")
+                            && !String.valueOf(sBuilder.charAt(c + 1)).equals("e")) {
+                        sBuilder.replace(c, c + 1, "k");
+                    } else if (String.valueOf(sBuilder.charAt(c + 1)).equals("i")) {
+                        sBuilder.replace(c, c + 2, "si");
+                    } else if (String.valueOf(sBuilder.charAt(c + 1)).equals("e")) {
+                        sBuilder.replace(c, c + 2, "se");
+                    }
+                } else {
+                    totalString.append(sBuilder + " ");
+                    break;
+                }
+
+            }
+        }
+        return totalString;
+    }
+
+    private StringBuilder checkDouble(String[] withoutC){
+        StringBuilder totalString = new StringBuilder();
+        for (int i = 0; i < withoutC.length; i++) {
+            StringBuilder sBuilder = new StringBuilder(withoutC[i]);
+            while (true){
+                String checkWord = sBuilder.toString();
+                for (int j = 0; j < sBuilder.length(); j++) {
+                    sBuilder.charAt(i);
+                    if (j == sBuilder.length()-1){
+                        totalString.append(sBuilder+" ");
+                        break;
+                    }
+                    if (String.valueOf(sBuilder.charAt(j))
+                            .equals(String.valueOf(sBuilder.charAt(j+1)))){
+                        if (String.valueOf(sBuilder.charAt(j)).equals("e")){
+                            sBuilder.replace(j,j+2,"i");
+                        }else if (String.valueOf(sBuilder.charAt(j)).equals("o")){
+                            sBuilder.replace(j,j+2,"u");
+                        }else {
+                            sBuilder.replace(j,j+2,String.valueOf(sBuilder.charAt(j)));
+                        }
+                    }
+                }
+                if (checkWord.equals(sBuilder.toString())){
+                    break;
+                }
+            }
+        }
+        return totalString;
+    }
+
+    private StringBuilder removerLastLetter(String[] doubleRemovedText) {
+        StringBuilder totalString = new StringBuilder();
+        for (int i = 0; i < doubleRemovedText.length; i++) {
+            StringBuilder sBuilder = new StringBuilder(doubleRemovedText[i]);
+            String lastChar = String.valueOf(sBuilder.charAt(sBuilder.length()-1));
+
+            if (sBuilder.length() > 1
+                    && lastChar.equals("e")){
+                sBuilder.deleteCharAt(sBuilder.length()-1);
+            }
+            totalString.append(sBuilder+" ");
+        }
+        return totalString;
+    }
+
+
+    private StringBuilder removeArticles(String text, StringBuilder textWithNoE) {
+        String[] inputTextArray = text.split(" ");
+        List<Integer> integers = new ArrayList<>(inputTextArray.length);
+        for (int i = 0; i < inputTextArray.length; i++) {
+            if (inputTextArray[i].equals(A)
+                    || inputTextArray[i].equals(AN)
+                    || inputTextArray[i].equals(THE)){
+                integers.add(i);
+            }
+        }
+
+        String[] finalArray = textWithNoE.toString().split(" ");
+        List<String> stringList = Arrays.asList(finalArray);
+        integers.forEach(stringList::remove);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String s: stringList) {
+            stringBuilder.append(s+" ");
+        }
+        return stringBuilder;
+    }
+
+}
---
+++ src/example/TestProjectITRex/test1/View.java
@@ -0,0 +1,50 @@
+package example.TestProjectITRex.test1;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class View {
+
+    public void start(){
+        BufferedReader br = null;
+
+        try{
+
+            br = new BufferedReader(new InputStreamReader(System.in));
+
+            while (true) {
+
+                System.out.print("Enter something : ");
+                String input = br.readLine();
+
+
+
+                if ("q".equals(input)) {
+                    System.out.println("Exit!");
+                    System.exit(0);
+                }else {
+                    Logic logic = new Logic(input);
+                    String s = logic.doJob();
+
+                    System.out.println("-----------\n");
+                    System.out.println(s);
+                    System.out.println("-----------\n");
+                    System.exit(0);
+                }
+            }
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
